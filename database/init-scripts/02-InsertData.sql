4fca845f4b469d95f0fcbd3769ccb14442320f31
Joseph Beaumont <joseph.beaumont@efrei.net>
2024-12-14 23:09:17 +0100

    Initial commit for the project

---
+++ database/init-scripts/02-InsertData.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (name) VALUES ('IRC'), ('ETI'), ('CGP');
+INSERT INTO students (department_id, first_name, last_name)
+VALUES
+    (1, 'Eli', 'Copter'),
+    (2, 'Emma', 'Carter'),
+    (3, 'Aude', 'Java');
